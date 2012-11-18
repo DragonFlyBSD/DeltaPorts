167d49bf043e4203691f37731ad253ab1faad51a
John Marino <draco@marino.st>
2012-11-18 10:05:58 +0100

    WIP - more zfs extraction

--- ports/ports-mgmt/poudriere/dragonfly/patch-src_poudriere.d_jail.sh
+++ ports/ports-mgmt/poudriere/dragonfly/patch-src_poudriere.d_jail.sh
@@ -1,6 +1,37 @@
 --- src/poudriere.d/jail.sh.orig	2012-10-15 18:18:18.000000000 +0200
-+++ src/poudriere.d/jail.sh	2012-11-17 19:27:20.000000000 +0100
-@@ -68,7 +68,7 @@
++++ src/poudriere.d/jail.sh	2012-11-17 22:36:28.000000000 +0100
+@@ -37,28 +37,11 @@
+ 	nbs=$(zget stats_skipped|sed -e 's/ //g')
+ 	nbq=$(zget stats_queued|sed -e 's/ //g')
+ 	tobuild=$((nbq - nbb - nbf - nbi - nbs))
+-	zfs list -H -o ${NS}:type,${NS}:name,${NS}:version,${NS}:arch,${NS}:stats_built,${NS}:stats_failed,${NS}:stats_ignored,${NS}:stats_skipped,${NS}:status,${NS}:method ${JAILFS}| \
+-		awk -v q="$nbq" -v tb="$tobuild" '/^rootfs/  {
+-			print "Jailname: " $2;
+-			print "FreeBSD version: " $3;
+-			print "FreeBSD arch: "$4;
+-			print "install/update method: "$10;
+-			print "Status: "$9;
+-			print "Packages built: "$5;
+-			print "Packages failed: "$6;
+-			print "Packages ignored: "$7;
+-			print "Packages skipped: "$8;
+-			print "Packages queued: "q;
+-			print "Packages to be built: "tb;
+-		}'
++	list_jail_info ${nbq} ${tobuild}
+ }
+ 
+ list_jail() {
+-	[ ${QUIET} -eq 0 ] && \
+-		printf '%-20s %-20s %-7s %-7s %-7s %-7s %-7s %-7s %-7s %s\n' "JAILNAME" "VERSION" "ARCH" "METHOD" "SUCCESS" "FAILED" "IGNORED" "SKIPPED" "QUEUED" "STATUS"
+-	zfs list -rt filesystem -H \
+-		-o ${NS}:type,${NS}:name,${NS}:version,${NS}:arch,${NS}:method,${NS}:stats_built,${NS}:stats_failed,${NS}:stats_ignored,${NS}:stats_skipped,${NS}:stats_queued,${NS}:status ${ZPOOL}${ZROOTFS} | \
+-		awk '$1 == "rootfs" { printf("%-20s %-20s %-7s %-7s %-7s %-7s %-7s %-7s %-7s %s\n",$2, $3, $4, $5, $6, $7, $8, $9, $10, $11) }'
++	[ ${QUIET} -eq 0 ] && print_jails_table
+ }
+ 
+ delete_jail() {
+@@ -68,7 +51,7 @@
  		err 1 "Unable to remove jail ${JAILNAME}: it is running"
  
  	msg_n "Removing ${JAILNAME} jail..."
@@ -9,7 +40,7 @@
  	rmdir ${JAILMNT}
  	rm -rf ${POUDRIERE_DATA}/packages/${JAILNAME}
  	rm -rf ${POUDRIERE_DATA}/cache/${JAILNAME}
-@@ -106,22 +106,22 @@
+@@ -106,22 +89,22 @@
  			yes | injail env PAGER=/bin/cat /usr/sbin/freebsd-update install || err 1 "Fail to upgrade system"
  			zset version "${TORELEASE}"
  		fi
@@ -38,7 +69,7 @@
  		;;
  	allbsd)
  		err 1 "Upgrade is not supported with allbsd, to upgrade, please delete and recreate the jail"
-@@ -258,12 +258,10 @@
+@@ -258,12 +241,10 @@
  	test -z ${VERSION} && usage
  
  	if [ -z ${JAILMNT} ]; then
@@ -51,7 +82,7 @@
  		JAILFS=${ZPOOL}${ZROOTFS}/jails/${JAILNAME}
  	fi
  
-@@ -368,7 +366,7 @@
+@@ -368,7 +349,7 @@
  
  	jail -U root -c path=${JAILMNT} command=/sbin/ldconfig -m /lib /usr/lib /usr/lib/compat
  
--- ports/ports-mgmt/poudriere/dragonfly/patch-src_poudriere.d_ports.sh
+++ ports/ports-mgmt/poudriere/dragonfly/patch-src_poudriere.d_ports.sh
@@ -1,6 +1,18 @@
 --- src/poudriere.d/ports.sh.orig	2012-10-15 18:18:18.000000000 +0200
-+++ src/poudriere.d/ports.sh	2012-11-17 16:17:53.000000000 +0100
-@@ -116,7 +116,7 @@
++++ src/poudriere.d/ports.sh	2012-11-18 02:20:57.000000000 +0100
+@@ -92,10 +92,7 @@
+ esac
+ 
+ if [ ${LIST} -eq 1 ]; then
+-	[ $QUIET -eq 0 ] && \
+-		printf '%-20s %-10s\n' "PORTSTREE" "METHOD"
+-	zfs list -t filesystem -H -o ${NS}:type,${NS}:name,${NS}:method | \
+-		awk '$1 == "ports" {printf("%-20s %-10s\n",$2,$3) }'
++	[ ${QUIET} -eq 0 ] && print_ports_table
+ else
+ 	test -z "${PTNAME}" && usage
+ fi
+@@ -116,7 +113,7 @@
  *default delete use-rel-suffix
  ports-all" > ${PTMNT}/csup
  			csup -z -h ${CSUP_HOST} ${PTMNT}/csup || {
@@ -9,16 +21,21 @@
  				err 1 " Fail"
  			}
  			;;
-@@ -127,7 +127,7 @@
- 			/usr/sbin/portsnap -d ${PTMNT}/snap -p ${PTMNT}/ports fetch extract || \
- 			/usr/sbin/portsnap -d ${PTMNT}/snap -p ${PTMNT}/ports fetch extract || \
+@@ -124,10 +121,10 @@
+ 			mkdir ${PTMNT}/snap
+ 			msg "Extracting portstree \"${PTNAME}\"..."
+ 			mkdir ${PTMNT}/ports
+-			/usr/sbin/portsnap -d ${PTMNT}/snap -p ${PTMNT}/ports fetch extract || \
+-			/usr/sbin/portsnap -d ${PTMNT}/snap -p ${PTMNT}/ports fetch extract || \
++			portsnap -d ${PTMNT}/snap -p ${PTMNT}/ports fetch extract || \
++			portsnap -d ${PTMNT}/snap -p ${PTMNT}/ports fetch extract || \
  			{
 -				zfs destroy ${PTFS}
 +				zkill ${PTFS}
  				err 1 " Fail"
  			}
  			;;
-@@ -141,7 +141,7 @@
+@@ -141,7 +138,7 @@
  			msg_n "Checking out the ports tree..."
  			svn -q co ${proto}://${SVN_HOST}/ports/head \
  				${PTMNT} || {
@@ -27,7 +44,7 @@
  				err 1 " Fail"
  			}
  			echo " done"
-@@ -149,7 +149,7 @@
+@@ -149,7 +146,7 @@
  		git)
  			msg "Cloning the ports tree"
  			git clone ${GIT_URL} ${PTMNT} || {
@@ -36,7 +53,7 @@
  				err 1 " Fail"
  			}
  			echo " done"
-@@ -167,7 +167,7 @@
+@@ -167,7 +164,7 @@
  		&& err 1 "Ports tree \"${PTNAME}\" is currently mounted and being used."
  	msg "Deleting portstree \"${PTNAME}\""
  	PTFS=$(port_get_fs ${PTNAME})
