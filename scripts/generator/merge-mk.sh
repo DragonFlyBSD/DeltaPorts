c4f478eec31b3a92caf6234a15b53baca5a20a2d
Automaton <nobody@home.ok>
2013-03-25 01:34:08 +0100

    merge-mk script: only merge bsd.*.mk files

---
+++ scripts/generator/merge-mk.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#
+# Update $MERGED directory (ports + overlay = merged)
+#
+
+CONFFILE=/usr/local/etc/dports.conf
+
+if [ ! -f ${CONFFILE} ]; then
+   echo "Configuration file ${CONFFILE} not found"
+   exit 1
+fi
+
+checkdir ()
+{
+   eval "MYDIR=\$$1"
+   if [ ! -d ${MYDIR} ]; then
+     echo "The $1 directory (${MYDIR}) does not exist."
+     exit 1
+  fi
+}
+
+confopts=`grep "=" ${CONFFILE}`
+for opt in ${confopts}; do
+   eval $opt
+done
+
+checkdir DELTA
+checkdir FPORTS
+checkdir MERGED
+
+WORKAREA=/tmp/merge.workarea
+
+checkfirst=$(mount | grep ${WORKAREA})
+if [ -z "${checkfirst}" ]; then
+   rm -rf ${WORKAREA}
+   mkdir ${WORKAREA}
+   mount -t tmpfs tmpfs ${WORKAREA}
+fi
+
+rm -rf ${WORKAREA}/*
+
+for k in Mk Templates; do
+  cp -pR ${FPORTS}/${k} ${WORKAREA}/
+  diffs=$(find ${DELTA}/special/${k}/diffs -name \*\.diff)
+  for difffile in ${diffs}; do
+    patch --quiet -d ${WORKAREA}/${k} < ${difffile}
+  done
+  rm ${WORKAREA}/${k}/*.orig
+  cpdup -i0 ${WORKAREA}/${k} ${MERGED}/${k}
+done
+
+umount ${WORKAREA}
+rm -rf ${WORKAREA}
