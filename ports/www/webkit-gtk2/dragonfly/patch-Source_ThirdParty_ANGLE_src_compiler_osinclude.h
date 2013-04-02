b08b08347ea08e8454deec01bf71dafe7251da22
Automaton <nobody@home.ok>
2013-04-02 20:19:09 +0200

    webkit-gtk*: fix patch

--- ports/www/webkit-gtk2/dragonfly/patch-Source_ThirdParty_ANGLE_src_compiler_osinclude.h
+++ ports/www/webkit-gtk2/dragonfly/patch-Source_ThirdParty_ANGLE_src_compiler_osinclude.h
@@ -4,7 +4,7 @@
  #define ANGLE_OS_WIN
  #elif defined(__APPLE__) || defined(__linux__) || \
        defined(__FreeBSD__) || defined(__OpenBSD__) || \
-+      defined(__DragonFly__) \
++      defined(__DragonFly__) || \
        defined(__sun) || defined(ANDROID)
  #define ANGLE_OS_POSIX
  #else
