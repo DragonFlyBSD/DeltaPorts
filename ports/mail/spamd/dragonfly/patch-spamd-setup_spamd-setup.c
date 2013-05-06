5916762aae8c033fecd48f4b11810741f60c22c2
François Tigeot <ftigeot@wolfpond.org>
2013-05-06 23:46:53 +0200

    mail/spamd: Fix pfctl path

--- ports/mail/spamd/dragonfly/patch-spamd-setup_spamd-setup.c
+++ ports/mail/spamd/dragonfly/patch-spamd-setup_spamd-setup.c
@@ -1,5 +1,14 @@
 --- spamd-setup/spamd-setup.c.orig	2010-10-30 22:08:20.000000000 +0000
 +++ spamd-setup/spamd-setup.c
+@@ -52,7 +52,7 @@ int 		  ipfw_tabno = 2;
+ #endif
+ 
+ #define PATH_FTP		"/usr/bin/ftp"
+-#define PATH_PFCTL		"/sbin/pfctl"
++#define PATH_PFCTL		"/usr/sbin/pfctl"
+ #ifndef PATH_SPAMD_CONF
+ #define PATH_SPAMD_CONF		"/usr/local/etc/spamd/spamd.conf"
+ #endif
 @@ -98,7 +98,7 @@ int		 configure_spamd(u_short, char *, c
  int		 configure_pf(struct cidr *);
  int		 getlist(char **, char *, struct blacklist *, struct blacklist *);
