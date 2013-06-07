c2e9d3b9e24aa41d8e628cba51fed511e00501c8
John Marino <draco@marino.st>
2013-06-07 16:59:33 +0200

    scripts: add report to find ports that didn't build

---
+++ scripts/generator/report_unbuilt.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+# This script compares the DPORTS entries to the packages
+# created and generates a report listing all ports that did
+# not generate a package.
+#
+
+CONFFILE=/usr/local/etc/dports.conf
+
+if [ ! -f ${CONFFILE} ]; then
+   echo "Configuration file ${CONFFILE} not found"
+   exit 1
+fi
+
+checkdir ()
+{
+   eval "MYDIR=\$$1"
+   if [ ! -d ${MYDIR} ]; then
+     echo "The $1 directory (${MYDIR}) does not exist."
+     exit 1
+  fi
+}
+
+confopts=`grep "=" ${CONFFILE}`
+for opt in ${confopts}; do
+   eval $opt
+done
+
+checkdir DPORTS
+
+usage ()
+{
+   echo "'report_unbuilt <jail-portrees>'"
+   exit 1
+}
+
+if [ $# -ne 1 ]; then
+    usage
+fi
+
+
+EXCLUDE="^(Templates/|Tools/|Mk/)"
+PKGDIR=/usr/local/poudriere/data/packages/${1}/All
+
+if [ ! -d ${PKGDIR} ]; then
+    echo "The package directory does not exist"
+    exit 1
+fi
+
+portdirs=$(cd ${DPORTS}; find * -type d -depth 1 -maxdepth 1 | sort | grep -vE ${EXCLUDE})
+for portdir in ${portdirs}; do
+    cd ${DPORTS}/${portdir}
+    PN=$(make -V PKGNAME).txz
+    FULLPATH=${PKGDIR}/${PN}
+    if [ ! -f "${FULLPATH}" ]; then
+        echo ${portdir}
+    fi
+done
