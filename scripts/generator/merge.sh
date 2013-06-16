bcecff1936458c37e7a65d8809f2f06221f48cc8
DPorts Builder <nobody@home.ok>
2013-06-16 13:50:50 +0200

    merge script: try to fix LOCK

--- scripts/generator/merge.sh
+++ scripts/generator/merge.sh
@@ -171,7 +171,7 @@ while read fileline; do
       elif [ "${ML}" = "LOCK" ]; then
          # Locked and merged entry doesn't exist.  Copy from DPorts
          mkdir -p ${MERGED}/${val_1}
-         cpdup -VV -i0 ${DPORTS}/${val_1}/ ${MERGED}/${val_1}/
+         cpdup -VV -i0 ${DPORTS}/${val_1} ${MERGED}/${val_1}
       elif [ "${ML}" = "MASK" ]; then
          # remove if existed previously
          rm -rf ${MERGED}/${val_1}
