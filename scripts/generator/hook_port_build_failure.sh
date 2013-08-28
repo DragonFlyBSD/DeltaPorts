161736fb702cd61cd2d0cb05aaad4a811449368a
DPorts Builder <nobody@home.ok>
2013-08-28 00:02:02 -0700

    go ahead and marked failed for any reason

--- scripts/generator/hook_port_build_failure.sh
+++ scripts/generator/hook_port_build_failure.sh
@@ -11,14 +11,6 @@ SET_port_build_failure=fire_port_build_failure
 fire_port_build_failure() {
   [ "${3}" != "potential" ] && return
 
-  if [ "${5}" = "configure" \
-    -o "${5}" = "check-config" \
-    -o "${5}" = "depends" \
-    -o "${5}" = "patch" \
-    -o "${5}" = "build" \
-    -o "${5}" = "install" \
-    -o "${5}" = "package" ]; then
-
     cd ${4}
     local ARG2=$(make PORTSDIR=/home/automaton/DPorts -VPKGVERSION)
     local ARG1A=$(dirname ${4})
@@ -27,5 +19,4 @@ fire_port_build_failure() {
     local BASE=/usr/local/poudriere/data/logs/bulk/${2}/${3}
 
     /usr/local/etc/hooks_poudriere/failure_status.sh "${ARG1B}/${ARG1C}" "${ARG2}" "${BASE}"
-  fi
 }
