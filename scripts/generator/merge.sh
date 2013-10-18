648dbc4ecc1226b6fc99ed3ee8bdf9bc1a5ba190
DPorts Builder <nobody@home.ok>
2013-10-18 00:48:53 -0700

    move makefile generator to poudriere

--- scripts/generator/merge.sh
+++ scripts/generator/merge.sh
@@ -251,18 +251,3 @@ for lang in ${LANGS}; do
   cp ${FPORTS}/${lang}/Makefile.inc ${MERGED}/${lang}/Makefile.inc
 done
 
-CATEGORIES=$(cd ${MERGED} && find -s * -type d -depth 0 -maxdepth 0 -not \( -name Mk -o -name Tools -o -name Templates \) )
-
-rm -f ${MERGED}/Makefile
-for CAT in ${CATEGORIES}; do
-echo "SUBDIR += ${CAT}" >> ${MERGED}/Makefile
-done
-
-
-for CAT in ${CATEGORIES}; do
-   rm -f ${MERGED}/${CAT}/Makefile
-   PORTS=$(cd ${MERGED}/${CAT} && find -s * -type d -depth 0 -maxdepth 0)
-   for port in ${PORTS}; do
-      echo "SUBDIR += ${port}" >> ${MERGED}/${CAT}/Makefile
-   done
-done
