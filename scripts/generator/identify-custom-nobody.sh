7e652afe1fc6e39394405bf1994be050f1a094c0
DPorts Builder <nobody@home.ok>
2013-11-18 02:47:28 -0800

    improve ports@ search script

--- scripts/generator/identify-custom-nobody.sh
+++ scripts/generator/identify-custom-nobody.sh
@@ -32,16 +32,20 @@ AWKCMD2='{ print substr($2,12) }'
 checkdir FPORTS
 checkdir DELTA
 
-cd ${DELTA}/ports && find * -type d -name diffs -print | sed -e 's|/diffs||g' > ${TMPBASE}diffs
+cd ${DELTA}/ports && find * -type d -name diffs -depth 2 -print \
+  | sed -e 's|/diffs||g' > ${TMPBASE}diffs
+cd ${DELTA}/ports && find * -type d -name dragonfly -depth 2 -print \
+  | sed -e 's|/dragonfly||g' > ${TMPBASE}dfly
 cd ${DELTA}/ports && find * -type f -name Makefile.DragonFly -depth 2 -print \
   | sed -e 's|/Makefile.DragonFly||g' > ${TMPBASE}MDF
 
-cat ${TMPBASE}diffs ${TMPBASE}MDF | sort -u > ${TMPBASE}unique
+cat ${TMPBASE}diffs ${TMPBASE}MDF ${TMPBASE}dfly | sort -u > ${TMPBASE}unique
 
 while read line; do
 
 if [ -d ${FPORTS}/${line} ]; then
-cd ${FPORTS} && grep -l "^MAINTAINER=.*ports@FreeBSD.org" ${line}/Makefile*
+cd ${FPORTS} && grep -l "^MAINTAINER=.*ports@FreeBSD.org" ${line}/Makefile* \
+  | awk -F '/' '{print $1 "/" $2}'
 else
 echo ">>>>>>>>>>>>>>>>>>>>>>>>> DNE: ${line}"
 fi
