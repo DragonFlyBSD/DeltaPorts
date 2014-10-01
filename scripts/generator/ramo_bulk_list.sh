3f71645047ccacc826f9ebc460a34304b61f24f7
Automaton <nobody@home.ok>
2014-10-01 14:47:55 +0200

    combine quicksync with upstream script

--- scripts/generator/ramo_bulk_list.sh
+++ scripts/generator/ramo_bulk_list.sh
@@ -4,34 +4,13 @@
 # produce a list of ports that depend on that origin so they can build.
 # suitable for passing to poudriere's bulk -f function
 
-CONFFILE=/usr/local/etc/dports.conf
+. /usr/local/etc/dports.conf
+
 TMPFILE=/tmp/pre-bulk.list
 FINALFILE=/tmp/ramo.list
 
-if [ ! -f ${CONFFILE} ]; then
-   echo "Configuration file ${CONFFILE} not found"
-   exit 1
-fi
-
-checkdir ()
-{
-   eval "MYDIR=\$$1"
-   if [ ! -d ${MYDIR} ]; then
-     echo "The $1 directory (${MYDIR}) does not exist."
-     rm -f ${BUSYFILE}
-     exit 1
-  fi
-}
-
-confopts=`grep "=" ${CONFFILE}`
-for opt in ${confopts}; do
-   eval $opt
-done
-
 AWKCMD='{ print $1 }'
 AWKCMD2='{ print substr($2,12) }'
-
-checkdir MERGED
 
 [ -z "${1}" ] && echo "argument 1 must be port origin" && exit 1
 FULLLINE=$(grep "|/usr/ports/${1}|" ${INDEX} 2>/dev/null)
---
+++ scripts/generator/ramo_syncme_bulk_list.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Accept an origin as an argument (e.g editors/joe)
+# produce a list of ports that depend on that origin so they can build.
+# suitable for passing to poudriere's bulk -f function
+
+. /usr/local/etc/dports.conf
+
+SYNCFILE=/tmp/syncme
+TMPFILE=/tmp/ramo.list
+FINALFILE=/tmp/ramosync.list
+FULLPATH=$(pwd)/$0
+HERE=$(dirname ${FULLPATH})
+
+AWKCMD='{ print $1 }'
+AWKCMD2='{ print substr($2,12) }'
+
+if [ ! -f "${SYNCFILE}" ]; then
+   echo "The ${SYNCFILE} file does not exist"
+   exit 1
+fi
+
+rm -f ${FINALFILE}
+
+
+while read line; do
+   if [ -n "${line}" ]; then
+      echo processing ${line}
+      ${HERE}/ramo_bulk_list.sh ${line}
+      cat ${TMPFILE} >> ${FINALFILE}
+   fi
+done < ${SYNCFILE}
+rm ${TMPFILE}
