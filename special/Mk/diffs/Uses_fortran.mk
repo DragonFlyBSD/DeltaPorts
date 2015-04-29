e24e7841c9cea98cd9283f41d8527418874bc182
DPorts Builder <nobody@home.ok>
2015-04-29 00:55:44 -0700

    eliminate gcc47 for release 4.2

--- special/Mk/diffs/Uses_fortran.mk
+++ special/Mk/diffs/Uses_fortran.mk
@@ -1,13 +1,23 @@
---- Uses/fortran.mk.orig	2014-09-16 08:16:33.065616000 +0000
+--- Uses/fortran.mk.orig	2015-04-29 07:42:37 UTC
 +++ Uses/fortran.mk
-@@ -18,8 +18,8 @@ fortran_ARGS=	gcc
+@@ -16,15 +16,13 @@ fortran_ARGS=	gcc
+ .endif
+ 
  .if ${fortran_ARGS} == gcc
- .include "${PORTSDIR}/Mk/bsd.default-versions.mk"
- _GCC_VER=	${GCC_DEFAULT:S/.//}
+-.include "${PORTSDIR}/Mk/bsd.default-versions.mk"
+-_GCC_VER=	${GCC_DEFAULT:S/.//}
+-.if ${GCC_DEFAULT} == ${LANG_GCC_IS}
 -BUILD_DEPENDS+=	gfortran${_GCC_VER}:${PORTSDIR}/lang/gcc
 -RUN_DEPENDS+=	gfortran${_GCC_VER}:${PORTSDIR}/lang/gcc
-+BUILD_DEPENDS+=	gfortran${_GCC_VER}:${PORTSDIR}/lang/gcc${_GCC_VER}
-+RUN_DEPENDS+=	gfortran${_GCC_VER}:${PORTSDIR}/lang/gcc${_GCC_VER}
+-.else
++.  if ${DFLYVERSION} < 400105
++_GCC_VER=	47
++.  else
++_GCC_VER=	5
++.  endif
+ BUILD_DEPENDS+=	gfortran${_GCC_VER}:${PORTSDIR}/lang/gcc${_GCC_VER}
+ RUN_DEPENDS+=	gfortran${_GCC_VER}:${PORTSDIR}/lang/gcc${_GCC_VER}
+-.endif
  USE_BINUTILS=	yes
  F77=		gfortran${_GCC_VER}
  FC=		gfortran${_GCC_VER}
