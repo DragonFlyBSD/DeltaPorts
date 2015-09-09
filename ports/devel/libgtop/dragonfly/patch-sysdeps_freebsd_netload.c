590bd2a6b1b5a956da167c063f0bcaa79ebf5e75
DPorts Builder <nobody@home.ok>
2015-09-09 11:47:16 -0700

    regen for libgtp

--- ports/devel/libgtop/dragonfly/patch-sysdeps_freebsd_netload.c
+++ ports/devel/libgtop/dragonfly/patch-sysdeps_freebsd_netload.c
@@ -1,16 +1,15 @@
---- sysdeps/freebsd/netload.c.orig	2014-11-21 12:23:34 UTC
+--- sysdeps/freebsd/netload.c.orig	2015-09-09 18:30:41 UTC
 +++ sysdeps/freebsd/netload.c
-@@ -67,6 +67,7 @@ void
+@@ -70,6 +70,7 @@ void
  glibtop_get_netload_p (glibtop *server, glibtop_netload *buf,
                         const char *interface)
  {
 +#ifndef __DragonFly__ /* TODO */
- 	struct ifaddrs *ifap, *ifa;
+         struct ifaddrs *ifap, *ifa;
  
-         glibtop_init_p (server, (1L << GLIBTOP_SYSDEPS_NETLOAD), 0);
-@@ -192,4 +193,5 @@ glibtop_get_netload_p (glibtop *server,
- 	}
- 
- 	freeifaddrs(ifap);
-+#endif /* !__DragonFly__ */
+         memset (buf, 0, sizeof (glibtop_netload));
+@@ -192,3 +193,4 @@ glibtop_get_netload_p (glibtop *server,
+         }
+         freeifaddrs(ifap);
  }
++#endif /* !__DragonFly__ */
