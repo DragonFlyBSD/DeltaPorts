fce90fc6feb47516374a753fb886c3bd78cfb8f0
zrj <rimvydas.jasinskas@gmail.com>
2015-09-20 16:39:32 +0300

    math/ised: add cstring for strlen

---
+++ ports/math/ised/dragonfly/patch-src_ised.cpp
@@ -0,0 +1,10 @@
+--- src/ised.cpp.orig	2012-09-01 19:10:10.000000000 +0300
++++ src/ised.cpp
+@@ -16,6 +16,7 @@
+ **/
+ 
+ #include <cstdio>
++#include <cstring>
+ #include "vm.h"
+ 
+ #include <unistd.h>
