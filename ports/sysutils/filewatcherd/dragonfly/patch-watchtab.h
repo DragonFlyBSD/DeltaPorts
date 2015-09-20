e44b026368e3cb3dd56b2e44330c9c80b4db4ea4
zrj <rimvydas.jasinskas@gmail.com>
2015-09-20 19:02:46 +0300

    oops, git add cleaned patch

--- ports/sysutils/filewatcherd/dragonfly/patch-watchtab.h
+++ ports/sysutils/filewatcherd/dragonfly/patch-watchtab.h
@@ -1,5 +1,5 @@
---- /tmp/watchtab.h	2013-09-14 18:09:32.000000000 +0300
-+++ watchtab.h	2015-09-20 18:35:20.000000000 +0300
+--- watchtab.h.orig	2013-09-14 18:09:32.000000000 +0300
++++ watchtab.h
 @@ -23,6 +23,9 @@
  #include <sys/queue.h>
  #include <sys/types.h>
