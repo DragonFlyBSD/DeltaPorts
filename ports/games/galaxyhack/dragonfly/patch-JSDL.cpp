23a1533700c622735be4dd5c010270b065d248a9
zrj <rimvydas.jasinskas@gmail.com>
2015-09-20 19:13:54 +0300

    games/galaxyhack: add missing cstring for std::memcpy

---
+++ ports/games/galaxyhack/dragonfly/patch-JSDL.cpp
@@ -0,0 +1,10 @@
+--- JSDL.cpp.bak	2015-09-20 19:06:17.000000000 +0300
++++ JSDL.cpp
+@@ -18,6 +18,7 @@
+ #include <SDL_mixer.h>
+ 
+ #include <cstdlib>
++#include <cstring>
+ #include <fstream>
+ #include <stdexcept>
+ 
