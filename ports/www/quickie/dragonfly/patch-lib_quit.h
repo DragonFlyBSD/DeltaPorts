5d65f82a9d361c21a880e1882399f8b2acfe546f
zrj <rimvydas.jasinskas@gmail.com>
2015-10-05 14:57:29 +0300

    www/quickie: adjust

--- ports/www/quickie/dragonfly/patch-lib_quit.h
+++ ports/www/quickie/dragonfly/patch-lib_quit.h
@@ -1,12 +1,10 @@
 --- lib/quit.h.orig	2006-05-21 01:56:52.000000000 +0300
 +++ lib/quit.h
-@@ -25,6 +25,9 @@
+@@ -25,6 +25,7 @@
  #pragma interface "quit"
  
  #include <cstdarg>
-+#ifdef __DragonFly__
 +#include <cstring>
-+#endif
  #include <format_printf.h>
  #include <quit/atexit_list.h>
  
