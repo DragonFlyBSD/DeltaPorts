1c7a287e45a9bd59297a8935296146b3dfef5c15
zrj <rimvydas.jasinskas@gmail.com>
2015-12-06 17:51:01 +0200

    games/enigma: mark as broken
    segfaults with png > v1.5

---
+++ ports/games/enigma/dragonfly/patch-src__file.hh
@@ -0,0 +1,10 @@
+--- src/file.hh.orig	2007-09-08 15:20:02.000000000 +0300
++++ src/file.hh
+@@ -21,6 +21,7 @@
+ 
+ #include <iosfwd>
+ #include <vector>
++#include <memory>
+ #include <list>
+ #include "ecl_error.hh"
+ 
