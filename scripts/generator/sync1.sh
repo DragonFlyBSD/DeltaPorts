3a9b885f41fcc02e6bdd0e58cdeab1a6e0c56232
DPorts Builder <nobody@home.ok>
2016-08-20 09:47:50 -0700

    add BROKEN_* filtering to merge

--- scripts/generator/sync1.sh
+++ scripts/generator/sync1.sh
@@ -64,6 +64,7 @@ transform ()
       cat ${WORK}/${item} | sed -E \
 	 -e 's|OPTIONS_DEFAULT_amd64|OPTIONS_DEFAULT_x86_64|g' \
 	 -e 's|OPTIONS_DEFINE_amd64|OPTIONS_DEFINE_x86_64|g' \
+	 -e 's|BROKEN_amd64|BROKEN_x86_64|g' \
 	 -e 's|_ON_amd64|_ON_x86_64|g' \
 	 -e 's|_OFF_amd64|_OFF_x86_64|g' \
 	 -e 's|CFLAGS_amd64|CFLAGS_x86_64|g' \
