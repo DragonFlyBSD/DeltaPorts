1442dbb7dedd4a951bc6d26fbb9cc24a4ab5595c
John Marino <dragonflybsd@marino.st>
2016-10-01 07:50:50 -0500

    fix patch of gdb66 (fetch is broken though)

--- ports/devel/gdb66/dragonfly/patch-include_libiberty.h
+++ ports/devel/gdb66/dragonfly/patch-include_libiberty.h
@@ -1,4 +1,4 @@
---- include/libiberty.h.orig	2006-06-01 14:57:47.000000000 +0000
+--- include/libiberty.h.intermediate	2006-06-01 14:57:47.000000000 +0000
 +++ include/libiberty.h
 @@ -97,7 +97,7 @@ extern void expandargv PARAMS ((int *, c
     to find the declaration so provide a fully prototyped one.  If it
@@ -6,6 +6,6 @@
  #if !HAVE_DECL_BASENAME
 -#if defined (__GNU_LIBRARY__ ) || defined (__linux__) || defined (__FreeBSD__) || defined (__OpenBSD__) || defined(__NetBSD__) || defined (__CYGWIN__) || defined (__CYGWIN32__) || defined (__MINGW32__) || defined (HAVE_DECL_BASENAME)
 +#if defined (__GNU_LIBRARY__ ) || defined (__linux__) || defined (__FreeBSD__) || defined (__OpenBSD__) || defined(__NetBSD__) || defined (__CYGWIN__) || defined (__CYGWIN32__) || defined (__MINGW32__) || defined (HAVE_DECL_BASENAME) || defined(__DragonFly__)
- extern char *basename (const char *);
+ #include <libgen.h>
  #else
  /* Do not allow basename to be used if there is no prototype seen.  We
