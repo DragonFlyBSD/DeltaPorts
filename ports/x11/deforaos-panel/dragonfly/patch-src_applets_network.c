8c3682de3647b0dbaa5d765e380c0c4541b32e1d
zrj <rimvydas.jasinskas@gmail.com>
2017-01-26 09:05:22 +0200

    x11/deforaos-panel: Fix build and add more DragonFly support.
    For now skip swap info bits.

--- ports/x11/deforaos-panel/dragonfly/patch-src_applets_network.c
+++ ports/x11/deforaos-panel/dragonfly/patch-src_applets_network.c
@@ -1,10 +1,20 @@
---- src/applets/network.c.orig	2014-10-23 17:34:42 UTC
+--- src/applets/network.c.orig	2016-01-12 03:32:16.000000000 +0200
 +++ src/applets/network.c
-@@ -32,6 +32,7 @@
- #include "Panel.h"
- #define _(string) gettext(string)
- 
-+#undef SIOCGIFDATA
- 
- /* Network */
- /* private */
+@@ -294,7 +294,7 @@ static void _refresh_interface_flags(Net
+ {
+ 	gboolean active = TRUE;
+ 	char const * icon = "network-offline";
+-#ifdef SIOCGIFDATA
++#if defined(SIOCGIFDATA) && !defined(__DragonFly__)
+ 	struct ifdatareq ifdr;
+ # if GTK_CHECK_VERSION(2, 12, 0)
+ 	unsigned long ibytes;
+@@ -309,7 +309,7 @@ static void _refresh_interface_flags(Net
+ 	else
+ #endif
+ 	{
+-#ifdef SIOCGIFDATA
++#if defined(SIOCGIFDATA) && !defined(__DragonFly__)
+ 		/* XXX ignore errors */
+ 		memset(&ifdr, 0, sizeof(ifdr));
+ 		strncpy(ifdr.ifdr_name, ni->name, sizeof(ifdr.ifdr_name));
