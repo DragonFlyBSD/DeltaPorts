2acfd802b468311091f56843bcee101a8ebe851b
John Marino <dragonflybsd@marino.st>
2017-04-02 15:41:21 -0500

    rework

--- ports/net/cjdns/dragonfly/patch-crypto_random_seed_BsdKernArndSysctlRandomSeed.h
+++ ports/net/cjdns/dragonfly/patch-crypto_random_seed_BsdKernArndSysctlRandomSeed.h
@@ -6,8 +6,8 @@ No KERN_ARND on DragonFly yet?
  #include "memory/Allocator.h"
  #include "util/Linker.h"
  
--#if defined(freebsd) || !defined(__DragonFly__)
-+#if defined(freebsd)
+-#if defined(freebsd)
++#if defined(freebsd) || !defined(__DragonFly__)
      Linker_require("crypto/random/seed/BsdKernArndSysctlRandomSeed.c");
      struct RandomSeed* BsdKernArndSysctlRandomSeed_new(struct Allocator* alloc);
      RandomSeedProvider_register(BsdKernArndSysctlRandomSeed_new)
