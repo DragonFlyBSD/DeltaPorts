30b381089c52cc221af2195f675ce4a85f055a85
John Marino <dragonflybsd@marino.st>
2017-07-20 13:03:15 -0500

    catchall _amd64= for merge

--- scripts/generator/merge.sh
+++ scripts/generator/merge.sh
@@ -72,6 +72,7 @@ transform ()
 	 -e 's|_OFF_amd64|_OFF_x86_64|g' \
 	 -e 's|CFLAGS_amd64|CFLAGS_x86_64|g' \
 	 -e 's|{ARCH:Mamd64}|{ARCH:Mx86_64}|g' \
+	 -e 's|_amd64=|_x86_64=|g' \
          -e '/ARCH}.*(amd64|"amd64")/s|amd64|x86_64|g' \
          > ${WORK}/${item}.filtered
       touch -r ${WORK}/${item} ${WORK}/${item}.filtered
