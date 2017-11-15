0ae79087eb9cd002295c0baa76aed0e93bacacad
DPorts Builder <nobody@home.ok>
2017-11-15 06:29:36 -0800

    regen llvm-cheri patches

--- ports/devel/llvm-cheri/dragonfly/patch-cmake_modules_HandleLLVMOptions.cmake
+++ ports/devel/llvm-cheri/dragonfly/patch-cmake_modules_HandleLLVMOptions.cmake
@@ -1,11 +1,11 @@
---- cmake/modules/HandleLLVMOptions.cmake.orig	2016-05-12 13:18:31.000000000 +0300
+--- cmake/modules/HandleLLVMOptions.cmake.orig	2017-10-05 11:03:27 UTC
 +++ cmake/modules/HandleLLVMOptions.cmake
-@@ -132,7 +132,7 @@ endif()
+@@ -141,7 +141,7 @@ endif()
  # Pass -Wl,-z,defs. This makes sure all symbols are defined. Otherwise a DSO
  # build might work on ELF but fail on MachO/COFF.
  if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin" OR WIN32 OR CYGWIN OR
--        ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD") AND
-+        ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD|DragonFly") AND
+-        ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD" OR
++        ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD|DragonFly" OR
+         ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD") AND
     NOT LLVM_USE_SANITIZER)
    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-z,defs")
- endif()
