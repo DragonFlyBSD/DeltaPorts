4653819a6756520a63e362b128842fb5b67b1e1e
DPorts Builder <nobody@home.ok>
2018-10-23 06:54:30 -0700

    try to fix qt5-network with libressl

--- ports/net/qt5-network/dragonfly/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
+++ ports/net/qt5-network/dragonfly/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
@@ -1,13 +1,11 @@
-Boilerplate for SSL_CTX_set1_groups() used in qsslcontext_openssl.cpp.
---- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2017-11-30 13:49:46 UTC
+--- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2018-09-13 04:25:10 UTC
 +++ src/network/ssl/qsslsocket_openssl_symbols_p.h
-@@ -454,6 +454,9 @@ void q_EC_KEY_free(EC_KEY *ecdh);
- size_t q_EC_get_builtin_curves(EC_builtin_curve *r, size_t nitems);
- #if OPENSSL_VERSION_NUMBER >= 0x10002000L
- int q_EC_curve_nist2nid(const char *name);
-+#if defined(LIBRESSL_VERSION_NUMBER)
-+int q_SSL_CTX_set1_groups(SSL_CTX *a, int *b, int c);
-+#endif // defined(LIBRESSL_VERSION_NUMBER)
- #endif // OPENSSL_VERSION_NUMBER >= 0x10002000L
- #endif // OPENSSL_NO_EC
- #if OPENSSL_VERSION_NUMBER >= 0x10002000L
+@@ -356,7 +356,7 @@ int q_SSL_CTX_use_PrivateKey(SSL_CTX *a,
+ int q_SSL_CTX_use_RSAPrivateKey(SSL_CTX *a, RSA *b);
+ int q_SSL_CTX_use_PrivateKey_file(SSL_CTX *a, const char *b, int c);
+ X509_STORE *q_SSL_CTX_get_cert_store(const SSL_CTX *a);
+-#if OPENSSL_VERSION_NUMBER >= 0x10002000L
++#if OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER)
+ SSL_CONF_CTX *q_SSL_CONF_CTX_new();
+ void q_SSL_CONF_CTX_free(SSL_CONF_CTX *a);
+ void q_SSL_CONF_CTX_set_ssl_ctx(SSL_CONF_CTX *a, SSL_CTX *b);
