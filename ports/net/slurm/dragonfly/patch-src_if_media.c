e3dfed1e3552c6d01043c60ba23284c231cd6293
DPorts Builder <nobody@home.ok>
2018-11-25 20:14:08 -0800

    some fixes

--- ports/net/slurm/dragonfly/patch-src_if_media.c
+++ ports/net/slurm/dragonfly/patch-src_if_media.c
@@ -1,4 +1,4 @@
---- src/if_media.c.bak	2015-10-04 11:38:57.000000000 +0300
+--- src/if_media.c.orig	2018-11-26 04:04:58 UTC
 +++ src/if_media.c
 @@ -9,11 +9,11 @@
   * $Id: if_media.c,v 1.35 2004/09/27 13:08:59 hscholz Exp $
@@ -32,23 +32,7 @@
          case IFM_HPNA_1:
  #else
  #if __FreeBSD__ <= 4
-@@ -169,6 +169,7 @@ int get_if_speed (char *ifstring)
-         /* fpa doesn't seem to support SIOCGIFMEDIA on FreeBSD
-          * so we won't get here but anyway ...
-          */
-+#ifndef __DragonFly__
-         case IFM_FDDI:
-             switch (physical)
-             {
-@@ -181,6 +182,7 @@ int get_if_speed (char *ifstring)
-                     speed = ERR_IFACE_NO_SPEED;
-             }
-             break;
-+#endif
- #if WIRELESS
-         /* IEEE 802.11 wireless interfaces */
-         case IFM_IEEE80211:
-@@ -200,14 +202,14 @@ int get_if_speed (char *ifstring)
+@@ -184,14 +184,14 @@ int get_if_speed (char *ifstring)
                  case IFM_IEEE80211_DS11:
                      speed = 11 * 1000;
                      break;
