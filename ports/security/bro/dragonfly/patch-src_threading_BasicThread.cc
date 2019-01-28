10fb3ab86125b40cbc84145c29e9b80da1bf52cc
DPorts Builder <nobody@home.ok>
2019-01-28 08:31:00 -0800

    repatches, fix ghc

--- ports/security/bro/dragonfly/patch-src_threading_BasicThread.cc
+++ ports/security/bro/dragonfly/patch-src_threading_BasicThread.cc
@@ -1,11 +1,11 @@
---- src/threading/BasicThread.cc.orig	2017-06-26 23:24:37 UTC
+--- src/threading/BasicThread.cc.orig	2018-12-19 16:36:16 UTC
 +++ src/threading/BasicThread.cc
-@@ -60,7 +60,7 @@ void BasicThread::SetOSName(const char*
+@@ -64,7 +64,7 @@ void BasicThread::SetOSName(const char*
  	pthread_setname_np(arg_name);
  #endif
  
--#ifdef FREEBSD
-+#if defined FREEBSD || defined DRAGONFLY
- 	pthread_set_name_np(thread.native_handle(), arg_name, arg_name);
+-#ifdef __FreeBSD__
++#ifdef __FreeBSD__ || defined __DragonFly__
+ 	pthread_set_name_np(thread.native_handle(), arg_name);
  #endif
  	}
