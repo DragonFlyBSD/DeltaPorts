48e59d64cda92ea549e1be14d1a2c6eb96dbd11d
zrj <rimvydas.jasinskas@gmail.com>
2019-05-10 00:14:15 +0300

    graphics/povray37: Regen patches.
    Drop 98

--- ports/graphics/povray37/dragonfly/patch-vfe_unix_platformbase.cpp
+++ ports/graphics/povray37/dragonfly/patch-vfe_unix_platformbase.cpp
@@ -1,6 +1,6 @@
---- vfe/unix/platformbase.cpp.orig	2013-06-06 20:41:36.818242000 +0000
+--- vfe/unix/platformbase.cpp.orig	2018-05-27 09:54:06 UTC
 +++ vfe/unix/platformbase.cpp
-@@ -191,7 +191,7 @@ namespace pov_base
+@@ -133,7 +133,7 @@ namespace pov_base
  	{
  #ifdef HAVE_CLOCK_GETTIME
  		struct timespec ts;
