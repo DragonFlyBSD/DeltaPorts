46c6776ea94a9eb60bcfefa484f0203ed1f447d4
zrj <rimvydas.jasinskas@gmail.com>
2019-09-01 20:42:25 +0300

    www/node10: Regen patches.

--- ports/www/node10/dragonfly/patch-tools_gyp_pylib_gyp_common.py
+++ ports/www/node10/dragonfly/patch-tools_gyp_pylib_gyp_common.py
@@ -1,11 +1,11 @@
---- tools/gyp/pylib/gyp/common.py.orig	2019-03-05 15:16:33 UTC
+--- tools/gyp/pylib/gyp/common.py.orig	2019-08-15 19:20:13 UTC
 +++ tools/gyp/pylib/gyp/common.py
-@@ -421,6 +421,8 @@ def GetFlavor(params):
+@@ -421,7 +421,7 @@ def GetFlavor(params):
      return flavors[sys.platform]
    if sys.platform.startswith('sunos'):
      return 'solaris'
-+  if sys.platform.startswith('dragon'):
-+    return 'freebsd'
-   if sys.platform.startswith('freebsd'):
+-  if sys.platform.startswith('freebsd'):
++  if sys.platform.startswith('freebsd') or sys.platform.startswith('dragonfly'):
      return 'freebsd'
    if sys.platform.startswith('openbsd'):
+     return 'openbsd'
