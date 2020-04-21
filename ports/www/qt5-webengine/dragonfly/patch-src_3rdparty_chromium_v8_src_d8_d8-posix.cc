c717ed35839d747ed9681eca49dcfc634e1b7ee4
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2020-04-21 21:28:27 +0200

    www/qt5-webengine: Adapt patches to the new version

--- ports/www/qt5-webengine/dragonfly/patch-src_3rdparty_chromium_v8_src_d8_d8-posix.cc
+++ ports/www/qt5-webengine/dragonfly/patch-src_3rdparty_chromium_v8_src_d8_d8-posix.cc
@@ -1,6 +1,6 @@
---- src/3rdparty/chromium/v8/src/d8-posix.cc.orig	2019-03-07 09:23:57 UTC
-+++ src/3rdparty/chromium/v8/src/d8-posix.cc
-@@ -363,7 +363,7 @@ static Local<Value> GetStdout(Isolate* i
+--- src/3rdparty/chromium/v8/src/d8/d8-posix.cc.orig	2019-11-27 21:12:25 UTC
++++ src/3rdparty/chromium/v8/src/d8/d8-posix.cc
+@@ -350,7 +350,7 @@ static Local<Value> GetStdout(Isolate* i
  // See http://code.google.com/p/v8/issues/detail?id=401.
  #if defined(WNOWAIT) && !defined(ANDROID) && !defined(__APPLE__) && \
      !defined(__NetBSD__) && !defined(__Fuchsia__)
