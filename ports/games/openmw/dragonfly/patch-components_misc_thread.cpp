73c1efd1a07e25d1a7bcb26c49a2cc70db33e3b2
zrj <rimvydas.jasinskas@gmail.com>
2022-01-27 08:00:56 +0000

    games/openmw: Add correct patch

--- ports/games/openmw/dragonfly/patch-components_misc_thread.cpp
+++ ports/games/openmw/dragonfly/patch-components_misc_thread.cpp
@@ -9,13 +9,15 @@
  
  #include <sys/types.h>
  #include <sys/rtprio.h>
-@@ -53,6 +53,9 @@ namespace Misc
+@@ -53,7 +53,11 @@ namespace Misc
          struct rtprio prio;
          prio.type = RTP_PRIO_IDLE;
          prio.prio = RTP_PRIO_MAX;
 +#ifdef __DragonFly__
-+#define rtprio_thread lwp_rtprio
-+#endif
++        if (lwp_rtprio(RTP_SET, 0, -1, &prio) == 0)
++#else
          if (rtprio_thread(RTP_SET, 0, &prio) == 0)
++#endif
              Log(Debug::Verbose) << "Using idle priority for thread=" << std::this_thread::get_id();
          else
+             Log(Debug::Warning) << "Failed to set idle priority for thread=" << std::this_thread::get_id() << ": " << std::strerror(errno);
