519bc816a4ef4ec1226c18179b14a8f4e1580113
zrj <rimvydas.jasinskas@gmail.com>
2022-01-30 19:52:27 +0000

    multimedia/minisatip: Regen patches
     Also fix apple case in another source.

--- ports/multimedia/minisatip/dragonfly/patch-src_socketworks.c
+++ ports/multimedia/minisatip/dragonfly/patch-src_socketworks.c
@@ -1,6 +1,8 @@
---- src/socketworks.c.orig	2021-09-12 22:54:16 UTC
+Use fallback for sendmmsg()
+
+--- src/socketworks.c.intermediate	2022-01-30 19:27:23.000000000 +0000
 +++ src/socketworks.c
-@@ -1217,7 +1217,7 @@ pthread_t get_socket_thread(int s_id) {
+@@ -1217,14 +1217,14 @@ pthread_t get_socket_thread(int s_id) {
  #undef DEFAULT_LOG
  #define DEFAULT_LOG LOG_SOCKET
  
@@ -9,3 +11,11 @@
  struct mmsghdr {
      struct msghdr msg_hdr; /* Message header */
      unsigned int msg_len;  /* Number of bytes transmitted */
+ };
+ #endif
+ 
+-#if defined(__APPLE__) || defined(__SH4__)
++#if defined(__APPLE__) || defined(__SH4__) || defined(__DragonFly__)
+ int sendmmsg0(int rsock, struct mmsghdr *msg, int len, int t) {
+     int i;
+     for (i = 0; i < len; i++)
