4ac34bb4dfe40926f67204399250f106513c1de5
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2022-09-03 16:28:34 +0200

    docs: Add small utility to manipulate synclog

---
+++ docs/synclog.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+#
+# Simple script to handle synclog
+#
+import importlib
+import argparse
+import json
+import sys
+
+
+try:
+    importlib.util.find_spec('tabulate')
+    from tabulate import tabulate
+except ImportError:
+    print('Please install the tabulate module')
+    sys.exit(255)
+
+def parse_cmdline_args():
+    parser = argparse.ArgumentParser(description='Synclog handling')
+
+    parser.add_argument('-r',
+                        dest='release',
+                        action="store",
+                        type=str,
+                        help='Specify a release'
+    )
+
+    parser.add_argument('--todo-show',
+                        action='store_true',
+                        help='Show To-Do List for a specific release'
+    )
+
+
+    return parser.parse_args()
+
+def load_synclog():
+    with open('./synclog.json') as f:
+        data = json.load(f)
+    return data
+
+def todolist_show(data):
+    details = list(
+        filter(
+            lambda r: r['id'] == args.release,
+            data['releases']
+        )
+    )
+
+    d = details[0]
+    print(
+        tabulate(
+            d['To-do'], headers={ 'port': 'Port',
+                                  'desc': 'Description',
+                                  'status': 'Status'}
+        )
+    )
+
+def main():
+    data = load_synclog()
+
+    if args.release == None:
+        # Grab current release
+        args.release = data['current']
+
+    if args.todo_show:
+        todolist_show(data)
+
+if __name__ == '__main__':
+    args = parse_cmdline_args()
+    main()
