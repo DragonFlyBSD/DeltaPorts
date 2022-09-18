774083edf12110b04243a1d36793b0ef6daed3ed
DPorts Builder <nobody@home.ok>
2022-09-18 05:24:05 -0700

    Add INDEX generator for ports

---
+++ scripts/generator/generate_ports_INDEX.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+MP=/usr/ports
+failed=""
+
+rm -f /tmp/INDEX /tmp/failed.ports
+
+/usr/bin/find -s ${MP}/[a-z]* -type d -depth 1 -maxdepth 1 | \
+  while read port
+  do
+      cd ${port}
+      out="$(/usr/bin/make PORTSDIR=/home/automaton/DPorts PORT_DBDIR=/tmp describe)"
+      if [ $? -eq 0 ]; then
+	  echo "${out}" >> /tmp/INDEX
+      else
+	  echo failed: ${port} >> /tmp/failed.ports
+      fi
+  done
+
+# Overwrite existing INDEX-3
+/bin/mv /tmp/INDEX ${MP}/INDEX-3
+
+# Report failures
+echo "Failed ports:"
+cat /tmp/failed.ports
