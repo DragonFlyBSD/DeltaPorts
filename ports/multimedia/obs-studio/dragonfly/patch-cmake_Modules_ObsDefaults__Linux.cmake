78233bb71511822852881797f844253fef6938aa
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2023-05-28 19:53:28 +0200

    multimedia/obs-studio: Fix patch phase but the libobsglad error still there

--- ports/multimedia/obs-studio/dragonfly/patch-cmake_Modules_ObsDefaults__Linux.cmake
+++ ports/multimedia/obs-studio/dragonfly/patch-cmake_Modules_ObsDefaults__Linux.cmake
@@ -1,9 +1,9 @@
---- cmake/Modules/ObsDefaults_Linux.cmake.orig	2022-11-05 18:07:13 UTC
+--- cmake/Modules/ObsDefaults_Linux.cmake.orig	2023-02-04 10:17:10 UTC
 +++ cmake/Modules/ObsDefaults_Linux.cmake
-@@ -122,7 +122,7 @@ macro(setup_obs_project)
-     set(CPACK_GENERATOR "DEB")
+@@ -121,7 +121,7 @@ macro(setup_obs_project)
      set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
      set(CPACK_SET_DESTDIR ON)
+     set(CPACK_DEBIAN_DEBUGINFO_PACKAGE ON)
 -  elseif(OS_FREEBSD)
 +  elseif(OS_FREEBSD OR OS_DRAGONFLY)
      option(ENABLE_CPACK_GENERATOR
