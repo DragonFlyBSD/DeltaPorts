d62761eb86bcdf73a161daf4a946005dee821454
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2023-06-23 01:30:40 +0200

    science/InsightToolkit: fix patch phase but build is still broken

--- ports/science/InsightToolkit/dragonfly/patch-Modules_IO_TransformBase_include_itkTransformIOBase.h
+++ ports/science/InsightToolkit/dragonfly/patch-Modules_IO_TransformBase_include_itkTransformIOBase.h
@@ -1,11 +1,11 @@
---- Modules/IO/TransformBase/include/itkTransformIOBase.h.orig	2017-06-02 16:43:23.000000000 +0300
+--- Modules/IO/TransformBase/include/itkTransformIOBase.h.orig	2022-11-24 13:15:41 UTC
 +++ Modules/IO/TransformBase/include/itkTransformIOBase.h
-@@ -28,7 +28,7 @@
+@@ -29,7 +29,7 @@
  #include <string>
  
  #ifndef ITKIOTransformBase_TEMPLATE_EXPORT
--  #if defined(ITK_TEMPLATE_VISIBILITY_DEFAULT) || defined(__linux__) && defined(ITK_BUILD_SHARED_LIBS)
-+  #if defined(ITK_TEMPLATE_VISIBILITY_DEFAULT) || (defined(__DragonFly__) || defined(__linux__)) && defined(ITK_BUILD_SHARED_LIBS)
-     // Make everything visible
-     #define ITKIOTransformBase_TEMPLATE_EXPORT __attribute__ ((visibility ("default")))
-   #else
+-#  if defined(ITK_TEMPLATE_VISIBILITY_DEFAULT) || defined(__linux__) && defined(ITK_BUILD_SHARED_LIBS)
++#  if defined(ITK_TEMPLATE_VISIBILITY_DEFAULT) || (defined(__DragonFly__) || defined(__linux__)) && defined(ITK_BUILD_SHARED_LIBS)
+ // Make everything visible
+ #    define ITKIOTransformBase_TEMPLATE_EXPORT __attribute__((visibility("default")))
+ #  else
