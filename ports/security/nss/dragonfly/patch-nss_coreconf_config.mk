fc6b48d89e7bcffeef7a73252dce970a12987041
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2024-09-26 18:33:25 +0200

    security/nss: regen patches

---
+++ ports/security/nss/dragonfly/patch-nss_coreconf_config.mk
@@ -0,0 +1,11 @@
+--- nss/coreconf/config.mk.orig	2024-08-30 12:18:39 UTC
++++ nss/coreconf/config.mk
+@@ -31,7 +31,7 @@ endif
+ #######################################################################
+ 
+ TARGET_OSES = FreeBSD NetBSD OpenUNIX QNX Darwin OpenBSD \
+-              AIX WINNT Linux Android
++              AIX WINNT Linux Android DragonFly
+ 
+ ifeq (,$(filter-out $(TARGET_OSES),$(OS_TARGET)))
+ include $(CORE_DEPTH)/coreconf/$(OS_TARGET).mk
