a9c186db17bec7d437ac0d6876c7d57ee2bd8584
karthik-holla <karthikholla87@gmail.com>
2026-01-31 14:33:43 +0000

    security/gvm-libs: Regen patch

--- ports/security/gvm-libs/dragonfly/patch-boreas_ping.c
+++ ports/security/gvm-libs/dragonfly/patch-boreas_ping.c
@@ -1,6 +1,6 @@
---- boreas/ping.c.orig	2022-10-22 05:41:27 UTC
+--- boreas/ping.c.orig	2026-01-31 13:33:46 UTC
 +++ boreas/ping.c
-@@ -109,7 +109,7 @@ throttle (int soc, int so_sndbuf)
+@@ -105,7 +105,7 @@ throttle (int soc, int so_sndbuf)
    int cur_so_sendbuf = -1;
  
    /* Get the current size of the output queue size */
@@ -9,7 +9,7 @@
    if (ioctl (soc, TIOCOUTQ, &cur_so_sendbuf) == -1)
  #else
    if (ioctl (soc, SIOCOUTQ, &cur_so_sendbuf) == -1)
-@@ -129,7 +129,7 @@ throttle (int soc, int so_sndbuf)
+@@ -125,7 +125,7 @@ throttle (int soc, int so_sndbuf)
        while (cur_so_sendbuf >= so_sndbuf)
          {
            usleep (100000);
@@ -18,30 +18,33 @@
            if (ioctl (soc, TIOCOUTQ, &cur_so_sendbuf) == -1)
  #else
            if (ioctl (soc, SIOCOUTQ, &cur_so_sendbuf) == -1)
-@@ -213,6 +213,13 @@ send_icmp_v4 (int soc, struct in_addr *d
+@@ -209,9 +209,13 @@ send_icmp_v4 (int soc, struct in_addr *dst)
  
    int len;
    int datalen = 56;
-+#if defined(__DragonFly__)
+-#ifdef __FreeBSD__
+-  struct icmp *icmp;
+-#else
++  #if defined(__DragonFly__)
 +struct icmphdr { /* not available as partial struct from struct icmp */
 +	u_char	icmp_type;	/* type of message, see below */
 +	u_char	icmp_code;	/* type sub code */
 +	u_short	icmp_cksum;
 +};
-+#endif
++#endif 
    struct icmphdr *icmp;
+ #endif
  
-   /* Throttling related variables */
-@@ -220,7 +227,7 @@ send_icmp_v4 (int soc, struct in_addr *d
+@@ -219,7 +223,7 @@ send_icmp_v4 (int soc, struct in_addr *dst)
+   static int so_sndbuf = -1; // socket send buffer
    static int init = -1;
  
-   icmp = (struct icmphdr *) sendbuf;
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
+   icmp = (struct icmp *) sendbuf;
    icmp->icmp_type = ICMP_ECHO;
    icmp->icmp_code = 0;
- #else
-@@ -229,7 +236,7 @@ send_icmp_v4 (int soc, struct in_addr *d
+@@ -234,7 +238,7 @@ send_icmp_v4 (int soc, struct in_addr *dst)
  #endif
  
    len = 8 + datalen;
@@ -50,21 +53,23 @@
    icmp->icmp_cksum = 0;
    icmp->icmp_cksum = in_cksum ((u_short *) icmp, len);
  #else
-@@ -308,7 +315,7 @@ send_icmp (gpointer key, gpointer value,
+@@ -313,8 +317,8 @@ send_icmp (gpointer key, gpointer value, gpointer scan
          }
        else
          {
 -#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
-       dst4.s_addr = dst6_p->s6_addr[12];
+-          dst4.s_addr = dst6_p->s6_addr[12];
++#if defined(__FreeBSD__) || defined(__DragonFly__)  
++	dst4.s_addr = dst6_p->s6_addr[12];
  #else
-       dst4.s_addr = dst6_p->s6_addr32[3];
-@@ -577,7 +584,7 @@ send_tcp (gpointer key, gpointer value,
+           dst4.s_addr = dst6_p->s6_addr32[3];
+ #endif
+@@ -582,7 +586,7 @@ send_tcp (gpointer key, gpointer value, gpointer scann
      }
    else
      {
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
-       dst4.s_addr = dst6_p->s6_addr[12];
+        dst4.s_addr = dst6_p->s6_addr[12];
  #else
-       dst4.s_addr = dst6_p->s6_addr32[3];
+        dst4.s_addr = dst6_p->s6_addr32[3];
