8f0f82fcc83561e4dc124feb4eed087b4270eb95
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-05-13 01:55:32 +0200

    devel/readline: convert to DOPS (3)

--- ports/devel/readline/dragonfly/patch-terminal.c
+++ ports/devel/readline/dragonfly/patch-terminal.c
@@ -1,9 +1,10 @@
 Disable _rl_enable_bracketed_paste in case of cons25 too.
 Avoids echoing 2004h.
 
---- terminal.c.orig	2025-05-14 16:44:40 UTC
+--- terminal.c.orig
 +++ terminal.c
-@@ -580,7 +580,7 @@ _rl_init_terminal_io (const char *termin
+@@ -580,7 +580,7 @@
+     term = "dumb";
  
    _rl_term_isansi = RL_ANSI_TERM_DEFAULT;
 -  dumbterm = STREQ (term, "dumb") || STREQ (term, "vt52") || STREQ (term, "adm3a");
