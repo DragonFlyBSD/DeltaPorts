77e83d4264382f5d2576810c3b2597b08aa0dafc
LI Leding <lileding@gmail.com>
2026-09-15 13:29:29 +0800

    www/epiphany: integrate epiphany-50.4_3 for DragonFly
    Update Epiphany to 50.4 for GNOME 50 on DragonFly
    
    Consolidate the final port delta against upstream/master on the FreeBSD
    2026Q3 baseline, including reviewed overlay and source-patch corrections.

--- ports/www/epiphany/dragonfly/patch-lib_ephy-sync-utils.c
+++ ports/www/epiphany/dragonfly/patch-lib_ephy-sync-utils.c
@@ -1,11 +1,10 @@
---- lib/ephy-sync-utils.c.orig	2021-11-16 13:19:59.580937000 +0100
-+++ lib/ephy-sync-utils.c	2021-11-16 13:20:12.170656000 +0100
-@@ -30,7 +30,7 @@
- #include <stdio.h>
- #include <string.h>
- #include <webkit2/webkit2.h>
--#if defined(__linux__) || defined(__FreeBSD__)
-+#if defined(__linux__) || defined(__FreeBSD__) || defined(__DragonFly__)
+--- lib/ephy-sync-utils.c.orig
++++ lib/ephy-sync-utils.c
+@@ -33,3 +33,3 @@
+ #if defined(__linux__)
  #include <sys/random.h>
- #elif defined(__OpenBSD__)
- #include <unistd.h>
+-#elif defined(__FreeBSD__) || defined(__OpenBSD__)
++#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+@@ -185 +185 @@
+-#ifdef __OpenBSD__
++#if defined(__OpenBSD__) || defined(__DragonFly__)
