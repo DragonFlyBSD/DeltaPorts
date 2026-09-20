630d67a55aa1e25eb9a46abdc1b9d7c6ec46965f
LI Leding <lileding@gmail.com>
2026-09-20 21:44:48 +0800

    devel/dbus: integrate dbus-1.16.2_4,1 for DragonFly
    - Use kernel SCM_CREDS PID/UID instead of the PID-less LOCAL_PEERCRED path;
      stop reporting caller PID 0.

--- ports/devel/dbus/dragonfly/patch-dbus_dbus-sysdeps-unix.c
+++ ports/devel/dbus/dragonfly/patch-dbus_dbus-sysdeps-unix.c
@@ -1,28 +1,15 @@
---- dbus/dbus-sysdeps-unix.c.intermediate	2024-09-14 17:00:00 UTC
+--- dbus/dbus-sysdeps-unix.c.orig
 +++ dbus/dbus-sysdeps-unix.c
-@@ -60,7 +60,7 @@
- #include <grp.h>
- #include <arpa/inet.h>
- 
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
- #include <sys/ucred.h>
- #endif
- 
-@@ -2367,7 +2367,11 @@ _dbus_read_credentials_socket  (DBusSock
+@@ -2378,7 +2378,7 @@ _dbus_read_credentials_socket  (DBusSocket client_fd,
+         pid_read = cr.unp_pid;
+         uid_read = cr.unp_euid;
        }
-     else
-       {
-+#ifndef __DragonFly__
-         pid_read = cr.cr_pid;
-+#else
-+        pid_read = 0;
-+#endif
-         uid_read = cr.cr_uid;
-       }
- #elif defined(HAVE_CMSGCRED)
-@@ -2489,7 +2493,8 @@ _dbus_read_credentials_socket  (DBusSock
- #if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
+-#elif defined(LOCAL_PEERCRED)
++#elif defined(LOCAL_PEERCRED) && !defined(__DragonFly__)
+     struct xucred cr;
+     socklen_t cr_len = sizeof (cr);
+ 
+@@ -2513,6 +2513,7 @@ _dbus_read_credentials_socket  (DBusSocket client_fd,
      defined(__linux__) || \
      defined(__OpenBSD__) || \
 -    defined(__NetBSD__)
